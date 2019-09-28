8ed8f7b4e7cc867912eb2d56e5dc7be1458495d0
JhonathaNunes <jhonathasilveira@gmail.com>
2019-09-28 14:42:17 -0300

    Stub Autenticacao

--- desafio_rsi/__init__.py
+++ desafio_rsi/__init__.py
@@ -1,4 +1,19 @@
 from eve import Eve
+from eve.auth import BasicAuth
+from pymongo.collection import Collection
 
-app = Eve(__name__)
+app = None
+
+
+class Autenticacao(BasicAuth):
+    def check_auth(self, username, password, allowed_roles, resource, method):
+        clientes: Collection = app.data.driver.db['accounts']
+        res = clientes.find_one({
+            'cpf': username,
+            'password': password,
+        })
+        return res is not None
+
+
+app = Eve(__name__, auth=Autenticacao)
 application = app
--- desafio_rsi/__main__.py
+++ desafio_rsi/__main__.py
@@ -1,3 +1,3 @@
 from . import app
 
-app.run(debug=True)
+app.run()
