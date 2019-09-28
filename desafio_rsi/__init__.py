305200e68bff02f3a0cd7ac78aa5ed5c453f7b61
JhonathaNunes <jhonathasilveira@gmail.com>
2019-09-28 17:25:53 -0300

    Fix auth

--- desafio_rsi/__init__.py
+++ desafio_rsi/__init__.py
@@ -3,53 +3,51 @@ from eve.auth import BasicAuth
 from pymongo.collection import Collection
 from cerberus import Validator
 from flask import Flask, request, jsonify
-from .auth import Autenticacao, criptografrar_senha
+from .auth import Autenticacao, criptografrar_senha, esconder_senhas
 
 app = Eve(__name__, auth=Autenticacao)
 
 # pylint: disable=no-member
 app.on_insert_usuario += criptografrar_senha
 
-class Autenticacao(BasicAuth):
-    def check_auth(self, username, password, allowed_roles, resource, method):
-        clientes: Collection = app.data.driver.db['accounts']
-        res = clientes.find_one({
-            'cpf': username,
-            'password': password,
-        })
-        return res is not None
-
-
 app = Eve(__name__, auth=Autenticacao)
+
 
 @app.route('/conta/adicionarSaldo', methods=['POST'])
 def addValue():
     print(request.data)
     return jsonify({"message": "success"})
 
+
 @app.route('/transferir', methods=['POST'])
-def transferValue(accOrigin, accDest, value):
+def transferValue(request):
+    accOrigin = request.json['accOrigin']
+    accDest = request.json['accDest']
+    value = request.json['value']
     colecao_conta_origem: Collection = app.data.driver.db['conta']
     contaOrigem = colecao_conta_origem.find_one({
-            'id': accOrigin,
+        'id': accOrigin,
     })
     colecao_conta_destino: Collection = app.data.driver.db['conta']
     contaDestino = colecao_conta_destino.find_one({
-            'id': accDest,
+        'id': accDest,
     })
-    if contaOrigem is not None and contaDestino is not None value is not None:
+    if contaOrigem is not None and contaDestino is not None and value is not None:
         if contaOrigem["saldo"] < value:
-           return jsonify({"message": "Saldo insuficiente"})
+            return jsonify({"message": "Saldo insuficiente"})
         colecao_conta_origem.update_one(
             {"_id": contaOrigem['_id']},
             {"$set": {
                 'saldo': contaOrigem['saldo'] - value
-        }}, upsert=False)
+            }},
+            upsert=False)
         colecao_conta_destino.update_one(
             {"_id": accDest['_id']},
             {"$set": {
-            'saldo': contaOrigem['saldo'] + value
-        }}, upsert=False)
+                'saldo': contaOrigem['saldo'] + value
+            }},
+            upsert=False)
         return jsonify({"message": "success"})
+
 
 application = app
